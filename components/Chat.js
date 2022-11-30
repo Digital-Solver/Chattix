e0740e8b6009dd3e8f3d14009fc4d7cbd5278612
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-11-30 18:05:37 +0000

    Install firebase

--- components/Chat.js
+++ components/Chat.js
@@ -3,28 +3,30 @@ import { StyleSheet, Text, View, Platform, KeyboardAvoidingView } from 'react-na
 import React, { useEffect, useState } from 'react';
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
+// FIREBASE SETUP
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1kJ4i_DbJ4a1yNPf3zVA57M76ea6JXSs",
+  authDomain: "chattix-8a492.firebaseapp.com",
+  projectId: "chattix-8a492",
+  storageBucket: "chattix-8a492.appspot.com",
+  messagingSenderId: "420773651874",
+  appId: "1:420773651874:web:3d1c5f6737b1eb21e2befb"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+
 // Chat - component
-const Chat = (props) => {
+  const Chat = (props) => {
   const {name, activeColor} = props.route.params
 
-  const [messages, setMessages] =  useState([
-    {
-      _id: 1,
-      text: `Hello ${name}`,
-      createdAt: new Date(),
-      user: {
-        _id: 2,
-        name: "React Native",
-        avatar: "https://placeimg.com/140/140/any"
-      },
-    },
-    {
-      _id: 2,
-      text: `${name} has entered the chat.`,
-      createdAt: new Date(),
-      system: true,
-    }
-  ])
+  const referenceChatMessages = collection(db, 'messages');
+
+  const [messages, setMessages] =  useState([referenceChatMessages])
 
   // Sets the navigation header's title to the name prop
   useEffect(() => {props.navigation.setOptions({title: name})}, [name]);
