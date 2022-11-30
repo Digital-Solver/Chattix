2e4c26ae6bb7eff93510680e65d7f62e16d2a532
Kerr C <105001360+Digital-Solver@users.noreply.github.com>
2022-11-30 15:04:35 +0000

    Implement GiftedChat

--- components/Chat.js
+++ components/Chat.js
@@ -1,22 +1,63 @@
 // Dependencies
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, Platform, KeyboardAvoidingView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 // Chat - component
 const Chat = (props) => {
   const {name, activeColor} = props.route.params
 
-  const black = "#090C08"
-  const purple = "#474056"
-  const grey = "#B9C6AE"
-  const green = "#B9C6AE"
+  const [messages, setMessages] =  useState([
+    {
+      _id: 1,
+      text: `Hello ${name}`,
+      createdAt: new Date(),
+      user: {
+        _id: 2,
+        name: "React Native",
+        avatar: "https://placeimg.com/140/140/any"
+      },
+    },
+    {
+      _id: 2,
+      text: `${name} has entered the chat.`,
+      createdAt: new Date(),
+      system: true,
+    }
+  ])
 
   // Sets the navigation header's title to the name prop
   useEffect(() => {props.navigation.setOptions({title: name})}, [name]);
 
+  // Functions
+  const onSend = (messages = []) => {
+    setMessages((prevMessages) => GiftedChat.append(prevMessages, messages))
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble 
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          },
+        }}
+      />
+    );
+  }
+
   return (
     <View style={[styles.centeredContainer, {backgroundColor: activeColor}]}>
-      <Text style={{color: "white"}}>Your Name is: <Text style={{fontWeight: "bold", color: "white"}}>{name}</Text></Text>
+      <View style={styles.centeredContainer}>
+        <GiftedChat 
+        renderBubble={renderBubble.bind(Chat)}
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{_id: 1}}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behvaiour="height" /> : null }
+      </View>
     </View>
   )
 }
@@ -29,7 +70,5 @@ const styles = StyleSheet.create({
   centeredContainer: {
     flex: 1,
     flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 })
